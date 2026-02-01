c9753074ec749a8dfa5b925001f6a84807daa9e8
BL-Balaji <balaji@bridgelabz.com>
2026-02-01 10:00:00 +0530

    [Balaji]:UC4 - Add YARDS and CENTIMETERS to LengthUnit enum

--- src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -2,19 +2,20 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class QuantityMeasurementAppTest {
-    @Test void givenSameFeet_shouldBeEqual() {
-        assertEquals(new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET),
-                     new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET));
-    }
     @Test void given1FeetAnd12Inches_shouldBeEqual() {
         assertEquals(new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET),
                      new QuantityMeasurementApp.QuantityLength(12.0, QuantityMeasurementApp.LengthUnit.INCH));
     }
-    @Test void given1FeetAnd1Inch_shouldNotBeEqual() {
-        assertNotEquals(new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET),
-                        new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.INCH));
+    @Test void given1YardAnd3Feet_shouldBeEqual() {
+        assertEquals(new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS),
+                     new QuantityMeasurementApp.QuantityLength(3.0, QuantityMeasurementApp.LengthUnit.FEET));
     }
-    @Test void givenNullUnit_shouldThrowException() {
-        assertThrows(NullPointerException.class, () -> new QuantityMeasurementApp.QuantityLength(1.0, null));
+    @Test void given1YardAnd36Inches_shouldBeEqual() {
+        assertEquals(new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS),
+                     new QuantityMeasurementApp.QuantityLength(36.0, QuantityMeasurementApp.LengthUnit.INCH));
+    }
+    @Test void given2InchesAnd5_08Centimeters_shouldBeEqual() {
+        assertEquals(new QuantityMeasurementApp.QuantityLength(2.0, QuantityMeasurementApp.LengthUnit.INCH),
+                     new QuantityMeasurementApp.QuantityLength(5.08, QuantityMeasurementApp.LengthUnit.CENTIMETERS));
     }
 }
