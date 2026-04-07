ac8d3d70f268fa38f6f3424d5cc33ca309d1628a
BL-Balaji <balaji@bridgelabz.com>
2026-04-07 13:19:58 +0530

    [Balaji]:UC2 - Add Inches measurement equality, reduce dependency on main()

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -4,18 +4,29 @@ public class QuantityMeasurementApp {
     public static final class Feet {
         private final double value;
         public Feet(double value) { this.value = value; }
-        public double getValue() { return value; }
         @Override public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
+            return Double.compare(this.value, ((Feet) obj).value) == 0;
         }
         @Override public int hashCode() { return Objects.hash(value); }
         @Override public String toString() { return value + " ft"; }
     }
+    public static final class Inches {
+        private final double value;
+        public Inches(double value) { this.value = value; }
+        @Override public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            return Double.compare(this.value, ((Inches) obj).value) == 0;
+        }
+        @Override public int hashCode() { return Objects.hash(value); }
+        @Override public String toString() { return value + " in"; }
+    }
+    public static boolean areFeetEqual(double a, double b)  { return new Feet(a).equals(new Feet(b)); }
+    public static boolean areInchesEqual(double a, double b){ return new Inches(a).equals(new Inches(b)); }
     public static void main(String[] args) {
-        Feet a = new Feet(1.0); Feet b = new Feet(1.0);
-        System.out.println("Equal: " + a.equals(b));
+        System.out.println("Feet equal: "  + areFeetEqual(1.0, 1.0));
+        System.out.println("Inches equal: " + areInchesEqual(1.0, 1.0));
     }
 }
