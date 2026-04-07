ad558fc0f58bf782fb2bfc0a51148dc466efcce6
BL-Balaji <balaji@bridgelabz.com>
2026-04-07 13:22:52 +0530

    [Balaji]:UC7 - Add addition with explicit target unit specification

--- src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -2,24 +2,21 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class QuantityMeasurementAppTest {
-    @Test void given1FeetAnd1Feet_whenAdded_shouldReturn2Feet() {
+    @Test void given1FeetAnd12Inches_addInYards_shouldGiveCorrect() {
         QuantityMeasurementApp.QuantityLength a = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        QuantityMeasurementApp.QuantityLength b = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        assertEquals(2.0, a.add(b).getValue(), 1e-2);
+        QuantityMeasurementApp.QuantityLength b = new QuantityMeasurementApp.QuantityLength(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        QuantityMeasurementApp.QuantityLength result = a.add(b, QuantityMeasurementApp.LengthUnit.YARDS);
+        assertEquals(QuantityMeasurementApp.LengthUnit.YARDS, result.getUnit());
+        assertEquals(0.67, result.getValue(), 1e-2);
     }
-    @Test void given1FeetAnd12Inches_whenAdded_shouldReturn2Feet() {
+    @Test void given1FeetAnd12Inches_addInInches_shouldGive24() {
+        QuantityMeasurementApp.QuantityLength a = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        QuantityMeasurementApp.QuantityLength b = new QuantityMeasurementApp.QuantityLength(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        assertEquals(24.0, a.add(b, QuantityMeasurementApp.LengthUnit.INCH).getValue(), 1e-2);
+    }
+    @Test void given1FeetAnd12Inches_addDefaultUnit_shouldGive2Feet() {
         QuantityMeasurementApp.QuantityLength a = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET);
         QuantityMeasurementApp.QuantityLength b = new QuantityMeasurementApp.QuantityLength(12.0, QuantityMeasurementApp.LengthUnit.INCH);
         assertEquals(2.0, a.add(b).getValue(), 1e-2);
-        assertEquals(QuantityMeasurementApp.LengthUnit.FEET, a.add(b).getUnit());
-    }
-    @Test void given3InchesAnd3Inches_whenAdded_shouldReturn6Inches() {
-        QuantityMeasurementApp.QuantityLength a = new QuantityMeasurementApp.QuantityLength(3.0, QuantityMeasurementApp.LengthUnit.INCH);
-        QuantityMeasurementApp.QuantityLength b = new QuantityMeasurementApp.QuantityLength(3.0, QuantityMeasurementApp.LengthUnit.INCH);
-        assertEquals(6.0, a.add(b).getValue(), 1e-2);
-    }
-    @Test void givenAddWithNull_shouldThrowException() {
-        QuantityMeasurementApp.QuantityLength a = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        assertThrows(NullPointerException.class, () -> a.add(null));
     }
 }
